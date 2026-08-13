85bbdddf8819132932c17b30f2ef9c0a69be9b07
mustafa1we <Fv7m0ha5j.vr1@gmail.com>
2026-08-13 13:56:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -398,7 +398,7 @@ def main():
         except Exception:
             logging.exception("Unexpected polling error")
             time.sleep(5)
-            
+
 
 if __name__ == "__main__":
     main()
